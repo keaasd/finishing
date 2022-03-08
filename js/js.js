4210361bcd98eb616f46f95cb3013a0c00c6339d
“Евгений <kea-asd@yandex.ru>
2022-03-08 23:48:35 +0700

    Open modal

---
+++ js/js.js
@@ -0,0 +1,8 @@
+
+console.log('go: ');
+function openModal() {
+    document.getElementById("modal").style.left = "0px";
+}
+function closeModal() {
+    document.getElementById("modal").style.left = "-500%";
+}
